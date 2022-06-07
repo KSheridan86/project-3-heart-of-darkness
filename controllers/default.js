4d5beece1714dc4bd2ba069b0bf56cff2ea9a7ae
Ken Sheridan <kensheridan86@gmail.com>
2022-06-07 18:29:25 +0000

    terminal window made taller

--- controllers/default.js
+++ controllers/default.js
@@ -19,7 +19,7 @@ function socket() {
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
             cols: 80,
-            rows: 30,
+            rows: 35,
             cwd: process.env.PWD,
             env: process.env
         });
