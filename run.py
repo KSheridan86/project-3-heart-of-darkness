fcfe4e68335e19a631ce041700e93f393289c027
Ken Sheridan <kensheridan86@gmail.com>
2022-06-07 15:56:08 +0000

    added module docstring and import modules

---
+++ run.py
@@ -0,0 +1,18 @@
+"""
+Choose your own adventure game.
+This is a choose your own adventure game, based loosely on the theme's of
+the movie Apocalypse now and the inspiration for that movie, the book
+The Heart of Darkness by Joseph Conrad.
+You must navigate your way down the Congo river at the height of the
+Belgian colonial conquest.
+"""
+import time
+import random
+import sys
+import os
+import colorama
+from colorama import init
+from colorama import Fore, Style
+import narrative
+import ascii_art
+init()
