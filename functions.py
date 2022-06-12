6c222b6e0d803407d42f698a61d3838170a36746
Ken Sheridan <kensheridan86@gmail.com>
2022-06-12 10:54:50 +0000

    print text speed altered for better readability

--- functions.py
+++ functions.py
@@ -193,7 +193,7 @@ def txt_effect(text_to_print):
     for character in text_to_print:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.0005)
+        time.sleep(0.05)
     # print(text_to_print)
 
 
